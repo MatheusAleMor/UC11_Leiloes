3e5820d95cb22b4dfe168bad96524f9e06c08ab0
Matheus Moreira <matheus_amoreira@hotmail.com>
2023-07-27 23:41:32 -0300

    Tela 'Vendas' funcional

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -69,7 +69,7 @@ public class ProdutosDAO {
             System.out.println("Erro ao conectar: " + ex.getMessage());
         }
     }
-    
+
     public List<ProdutosDTO> listarProdutos() {
         String sql = "SELECT * FROM produtos";
         try {
@@ -90,5 +90,26 @@ public class ProdutosDAO {
             return null;
         }
     }
+
+    /*public List<ProdutosDTO> listarProdutosVendidos() {
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        try {
+            PreparedStatement st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            List<ProdutosDTO> listaProdutos = new ArrayList<>();
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                listaProdutos.add(produto);
+            }
+            return listaProdutos;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return null;
+        }
+    }*/
 
 }
